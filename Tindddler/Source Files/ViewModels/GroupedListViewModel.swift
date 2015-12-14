58e7ab69bbfa491167cde525826e6e26caf2ca17
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-14 11:33:58 +0100

    add basic view models files
    [IOS-69]

---
+++ Tindddler/Source Files/ViewModels/GroupedListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  GroupedListViewModel.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 14/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
