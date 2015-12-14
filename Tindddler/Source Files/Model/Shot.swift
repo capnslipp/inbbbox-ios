3ec81250e5ab156f865d21391254d009b15aa8e6
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-14 17:04:39 +0100

    add Shot model
    [IOS-63]

---
+++ Tindddler/Source Files/Model/Shot.swift
@@ -0,0 +1,33 @@
+//
+//  Shot.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 14/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Shot {
+    let identifier: String
+    let title: String?
+    let description: String?
+}
+
+extension Shot: Mappable {
+    static var map: JSON -> Shot {
+        return { json in
+            Shot(identifier: json[Key.Identifier.rawValue].stringValue,
+                title: json[Key.Title.rawValue].string,
+                description: json[Key.Description.rawValue].string
+            )
+        }
+    }
+
+    private enum Key: String {
+        case Identifier = "id"
+        case Title = "title"
+        case Description = "description"
+    }
+}
