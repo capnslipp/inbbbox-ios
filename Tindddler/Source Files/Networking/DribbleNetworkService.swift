dffa9047153f7c85d1eecaecd5c7dd4902f9aa53
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-14 15:10:14 +0100

    add Dribble service
    [IOS-63]

---
+++ Tindddler/Source Files/Networking/DribbleNetworkService.swift
@@ -0,0 +1,25 @@
+//
+//  DribbleNetworkService.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 14/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+struct DribbleNetworkService: SecureNetworkService, HeaderAuthorizable {
+    
+    let host = ""
+    
+    func authorizeRequest(request: NSMutableURLRequest) {
+        let header = authorizationHeader(TokenStorage.currentToken)
+        request.setHeader(header)
+    }
+}
+
+extension NSMutableURLRequest {
+    func setHeader(header: HTTPHeader) {
+        setValue(header.value, forHTTPHeaderField: header.name)
+    }
+}
