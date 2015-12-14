c59218560ebe505dce37ebc607b9e3e8927b56a4
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-14 16:37:54 +0100

    add request
    [IOS-63]

---
+++ Tindddler/Source Files/Networking/Request.swift
@@ -0,0 +1,17 @@
+//
+//  Request.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 14/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  Request
+ *  Requestable value type
+ */
+struct Request: Requestable {
+    let query: Query
+}
