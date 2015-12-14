31eb18d6181e5149d236c63dff45602928b5dc1d
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-14 17:04:24 +0100

    add Mappable protocol
    [IOS-63]

---
+++ Tindddler/Source Files/Model/Mappable.swift
@@ -0,0 +1,14 @@
+//
+//  Mappable.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 14/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+protocol Mappable {
+    static var map: JSON -> Self { get }
+}
