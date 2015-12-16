033eb946641370ae33eafa2eae14b78505d6945c
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-12-16 13:29:17 +0100

    Further clean up. Replaced ViewController with temporary LoginViewController [IOS-58]

--- Tindddler/Source Files/AppDelegate.swift
+++ Tindddler/Source Files/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window!.rootViewController = ViewController()
+        window!.rootViewController = LoginViewController()
         window!.makeKeyAndVisible()
         
         return true
