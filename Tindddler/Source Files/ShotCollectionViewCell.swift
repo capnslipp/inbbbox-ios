f96f8ad3c30d4be7f5652f9a73ff30ad81c23816
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2015-12-17 11:44:23 +0100

    Created ShotCollectionViewCell

---
+++ Tindddler/Source Files/ShotCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+class ShotCollectionViewCell: UICollectionViewCell {
+
+//    MARK: - Life cycle
+
+    required override init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.backgroundColor = UIColor.greenColor()
+    }
+}
--- Tindddler/Source Files/ViewControllers/ShotsCollectionViewController.swift
+++ Tindddler/Source Files/ViewControllers/ShotsCollectionViewController.swift
@@ -6,11 +6,25 @@ import UIKit
 
 class ShotsCollectionViewController: UICollectionViewController {
 
+    let shotCollectionViewCellIdentifier = "ShotCollectionViewCellIdentifier"
+
 //    MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.registerClass(ShotCollectionViewCell.self, forCellWithReuseIdentifier: shotCollectionViewCellIdentifier)
     }
+
+//    MARK: - UICollectionViewDataSource
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCellWithReuseIdentifier(shotCollectionViewCellIdentifier, forIndexPath: indexPath)
+    }
+
 }
