9e7e31627c27d8c87a6db63ad26c3044a1a8f153
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-12-17 17:02:09 +0100

    add unlike query
    [IOS-67]

---
+++ Tindddler/Source Files/Networking/UnlikeQuery.swift
@@ -0,0 +1,21 @@
+//
+//  UnlikeQuery.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 17/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+struct UnlikeQuery: Query {
+    
+    let method = Method.DELETE
+    let path: String
+    let service: SecureNetworkService = DribbbleNetworkService()
+    var parameters = Parameters(encoding: .URL)
+    
+    init(shotID: String) {
+        path = "/shots/" + shotID + "/unlike"
+    }
+}
---
+++ Unit Tests/UnlikeQuerySpec.swift
@@ -0,0 +1,50 @@
+//
+//  UnlikeQuerySpec.swift
+//  Tindddler
+//
+//  Created by Radoslaw Szeja on 17/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+import Tindddler
+
+@testable import Tindddler
+
+class UnlikeQuerySpec: QuickSpec {
+    override func spec() {
+        
+        describe("when newly initialized with shot identifier") {
+            
+            var sut: UnlikeQuery!
+            
+            beforeEach {
+                sut = UnlikeQuery(shotID: "fixture.identifier")
+            }
+            
+            it("should have post method") {
+                expect(sut.method.rawValue).to(equal(Method.DELETE.rawValue))
+            }
+            
+            it("should have path with identifier") {
+                expect(sut.path).to(equal("/shots/fixture.identifier/unlike"))
+            }
+            
+            it("should have dribbble service") {
+                expect(sut.service is DribbbleNetworkService).to(beTrue())
+            }
+            
+            it("should have parameters with URL encoding") {
+                expect(sut.parameters.encoding).to(equal(Parameters.Encoding.URL))
+            }
+            
+            it("should have empty parameters") {
+                expect(sut.parameters.queryItems).to(beEmpty())
+            }
+            
+        }
+
+    }
+}
