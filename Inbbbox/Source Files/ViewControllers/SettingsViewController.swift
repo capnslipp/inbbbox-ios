03e7929d2a586fea304a8a6adb5c7aaf50b10128
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-18 10:32:07 +0100

    ViewModel and ViewController for Settings partially implemented [IOS-69]

--- Inbbbox/Source Files/ViewControllers/SettingsViewController.swift
+++ Inbbbox/Source Files/ViewControllers/SettingsViewController.swift
@@ -8,6 +8,156 @@
 
 import Foundation
 
-class SettingsViewController {
-    //NGRTodo: implement me!
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    private weak var aView: GroupedBaseTableView?
+    private var viewModel: SettingsViewModel!
+    
+    init() {
+        self.viewModel = SettingsViewModel()
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel.delegate = self
+        self.title = self.viewModel.title
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func loadView() {
+        aView = loadViewWithClass(GroupedBaseTableView.self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupBarButtons()
+        setupTableView()
+    }
+}
+
+// MARK ModelUpdatable
+
+extension SettingsViewController: ModelUpdatable {
+    
+    func didChangeItemsAtIndexPaths(indexPaths: [NSIndexPath]) {
+        // NGRTodo: implement me!
+    }
+    
+    func addedItemsAtIndexPaths(indexPaths: [NSIndexPath]) {
+        aView?.tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Fade)
+    }
+    
+    func removedItemsAtIndexPaths(indexPaths: [NSIndexPath]) {
+        aView?.tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Fade)
+    }
+}
+
+// MARK UITableViewDataSource
+
+extension SettingsViewController: UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return viewModel.sectionsCount()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel[section].count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // NGRTodo: implement me!
+        return UITableViewCell()
+    }
+}
+
+// MARK UITableViewDelegate
+
+extension SettingsViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        let item = viewModel[indexPath.section][indexPath.row]
+        
+        if let item = item as? DatePickerItem, cell = cell as? DatePickerCell {
+            item.bindDatePicker(cell.datePicker)
+        } else if let item = item as? SwitchItem, cell = cell as? SwitchCell {
+            item.bindSwitchControl(cell.switchControl)
+        } else if let item = item as? ButtonItem, cell = cell as? ButtonCell {
+            item.bindButtonControl(cell.buttonControl)
+        }
+    }
+    
+    func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        let cellIndexPath = tableView.indexPathForCell(cell) ?? indexPath
+        let section = cellIndexPath.section
+        let row = cellIndexPath.row
+        
+        if row < viewModel[section].count {
+            let item = viewModel[section][row]
+            
+            if let item = item as? SwitchItem, _ = cell as? SwitchCell {
+                item.unbindSwitchControl()
+            } else if let item = item as? DatePickerItem, _ = cell as? DatePickerCell {
+                item.unbindDatePicker()
+            } else if let item = item as? ButtonItem, _ = cell as? ButtonCell {
+                item.unbindButtonControl()
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        // NGRTodo: implement me!
+        return CGFloat()
+    }
+    
+    func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        // NGRTodo: implement me!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        viewModel.handleSelectionAtIndexPath(indexPath)
+        tableView.deselectRowIfSelectedAnimated(true)
+    }
+}
+
+// MARK: Private
+
+private extension SettingsViewController {
+    
+    // MARK: Configuration
+    
+    func setupTableView() {
+        let tableView = aView!.tableView
+        
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.registerClass(SwitchCell.self)
+        tableView.registerClass(DateCell.self)
+        tableView.registerClass(ButtonCell.self)
+        
+    }
+    
+    func setupBarButtons() {
+        // NGRTodo: implement me!
+    }
+}
+
+private extension UITableView {
+    
+    func cellForItemCategory(category: GroupItem.Category) -> UITableViewCell {
+        
+        switch category {
+        case .Date: return dequeueReusableCell(DateCell.self)
+        case .Picker: return dequeueReusableCell(DatePickerCell.self)
+        case .Action: return dequeueReusableCell(ButtonCell.self)
+        case .Boolean: return dequeueReusableCell(SwitchCell.self)
+        default: return UITableViewCell()
+        }
+    }
 }
--- Inbbbox/Source Files/ViewModels/SettingsViewModel.swift
+++ Inbbbox/Source Files/ViewModels/SettingsViewModel.swift
@@ -8,6 +8,126 @@
 
 import Foundation
 
-class SettingsViewModel {
-    //NGRTodo: implement me!
+protocol ModelUpdatable {
+    func didChangeItemsAtIndexPaths(indexPaths: [NSIndexPath])
+    func addedItemsAtIndexPaths(indexPaths: [NSIndexPath])
+    func removedItemsAtIndexPaths(indexPaths: [NSIndexPath])
+}
+
+class SettingsViewModel: GroupedListViewModel {
+    
+    var delegate: ModelUpdatable?
+    var title: String
+    
+    let reminderItem: SwitchItem
+    let reminderDateItem: DateItem
+    let logOutButtonItem: ButtonItem
+    
+    init() {
+        
+        // MARK: Parameters
+        
+        title = NSLocalizedString("Account", comment: "")
+        
+        let reminderTitle = NSLocalizedString("Enable daily reminder", comment: "")
+        let reminderDateTitle = NSLocalizedString("Send daily reminder at", comment: "")
+        let logOutTitle = NSLocalizedString("Log out", comment: "")
+        
+        // MARK: Create items
+        
+        reminderItem = SwitchItem(title: reminderTitle)
+        reminderDateItem = DateItem(title: reminderDateTitle)
+        logOutButtonItem = ButtonItem(title: logOutTitle)
+        
+        // MARK: Super init
+        
+        super.init(items: [
+            [reminderItem, reminderDateItem],
+            [logOutButtonItem]
+            ] as [[GroupItem]])
+        
+        // MARK: onValueChanged and onButtonTapped blocks
+        
+        reminderItem.onValueChanged = { [weak self] text in
+            // NGRTodo: implement me!
+        }
+        
+        reminderDateItem.onValueChanged = {date -> Void in
+            // NGRTodo: implement me!
+        }
+        
+        logOutButtonItem.onButtonTapped = { _ -> Void in
+            // NGRTodo: implement me!
+        }
+    }
+}
+
+// MARK: Date pickers handling
+
+extension SettingsViewModel {
+    
+    func handleSelectionAtIndexPath(indexPath: NSIndexPath) {
+        let item = self[indexPath.section][indexPath.row]
+        
+        if let item = item as? DateItem {
+            handleSelectionOfDateItem(item, atIndexPath: indexPath)
+        }
+    }
+    
+    // NGRTemp: temporary implementation
+    private func handleSelectionOfDateItem(item: DateItem, atIndexPath indexPath: NSIndexPath) {
+        
+        var dateItems: [DateItem] = []
+        let collapseIndexPaths = indexPathsForItemOfType(DatePickerItem.self)
+        
+        if let pickersIndexPaths = collapseIndexPaths {
+            
+            for pickerIndexPath in pickersIndexPaths {
+                
+                let section = pickerIndexPath.section
+                let row = pickerIndexPath.row
+                
+                if let pickerItem = self[section][row] as? DatePickerItem {
+                    pickerItem.unbindDatePicker()
+                }
+                
+                removeAtIndexPath(pickerIndexPath)
+                
+                if let dateItem = self[section][row-1] as? DateItem {
+                    dateItem.highlighted = false
+                    dateItems.append(dateItem)
+                }
+            }
+            
+            delegate?.removedItemsAtIndexPaths(pickersIndexPaths)
+            
+            if let reloadIndexPaths = indexPathsForItems(dateItems) {
+                delegate?.didChangeItemsAtIndexPaths(reloadIndexPaths)
+            }
+        }
+        
+        if !item.highlighted && !dateItems.contains(item) {
+            
+            item.highlighted = true
+            
+            let pickerItem = datePickerItemForDateItem(item)
+            
+            if var currentIndexPath = indexPathsForItems([item])?.first {
+                currentIndexPath = NSIndexPath(forRow: currentIndexPath.row+1, inSection: currentIndexPath.section)
+                addItem(pickerItem, atIndexPath: currentIndexPath)
+                delegate?.addedItemsAtIndexPaths([currentIndexPath])
+            }
+        }
+    }
+    
+    private func datePickerItemForDateItem(item: DateItem) -> DatePickerItem {
+        return DatePickerItem(date: item.date) { [weak self] date in
+            item.date = date
+            item.update()
+            
+            if let indexPaths = self?.indexPathsForItems([item]) {
+                self?.delegate?.didChangeItemsAtIndexPaths(indexPaths)
+            }
+        }
+    }
 }
