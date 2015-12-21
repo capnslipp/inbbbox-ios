e582ee2a6be7f099cf3b3638c9e98578840d11cd
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-21 08:42:51 +0100

    added empty class for managing notification {IOS-70}

---
+++ Inbbbox/Source Files/Managers/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 21/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+class NotificationManager {
+    // NGRTodo: implement me!
+}
