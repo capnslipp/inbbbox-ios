18226941d8f77f455651ae063e1caf7a5f70372a
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-21 11:56:10 +0100

    NotificationManager implemented [IOS-70]

--- Inbbbox/Source Files/Managers/NotificationManager.swift
+++ Inbbbox/Source Files/Managers/NotificationManager.swift
@@ -6,8 +6,59 @@
 //  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class NotificationManager {
-    // NGRTodo: implement me!
+final class NotificationManager {
+    
+    class func registerNotification(forUserID userID: String, time: NSDate) {
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: nil))
+        
+        unregisterNotification(forUserID: userID)
+        
+        createNotification(forUserID: userID, atTime: time)
+    }
+    
+    class func unregisterNotification(forUserID userID: String) {
+        
+        let registeredNotification = isNotificationRegistered(forUserID: userID)
+        
+        if registeredNotification.exists {
+            destroyNotification(notificationID: registeredNotification.notification!)
+        }
+    }
+}
+
+// MARK: Private
+
+private extension NotificationManager {
+    
+    private class func createNotification(forUserID userID: String, atTime: NSDate) {
+        
+        let localNotification = UILocalNotification()
+        localNotification.userInfo = ["notificationID": userID]
+        localNotification.fireDate = atTime
+        localNotification.alertBody = NSLocalizedString("Check Inbbbox!", comment: "") // NGRTemp: temp text
+        localNotification.alertAction = NSLocalizedString("Show", comment: "") // NGRTemp: temp text
+        localNotification.repeatInterval = .Day
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+    
+    private class func destroyNotification(notificationID notificationToDelete: UILocalNotification) {
+        UIApplication.sharedApplication().cancelLocalNotification(notificationToDelete)
+    }
+    
+    private class func isNotificationRegistered(forUserID userID: String) -> (exists: Bool, notification: UILocalNotification?) {
+        
+        for localNotification in UIApplication.sharedApplication().scheduledLocalNotifications! {
+            if let userInfo = localNotification.userInfo where localNotification.userInfo is [String: String] {
+                let notificationID = userInfo["notificationID"] as! String
+                if notificationID == userID {
+                    return (true, localNotification)
+                }
+            }
+        }
+        return (false, nil)
+    }
 }
