79f81935a71d2324ed4ca39f2c0d08f4d662eda2
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-21 13:43:04 +0100

    empty SegmentCell created [IOS-69]

---
+++ Inbbbox/Source Files/Views/SegmentCell.swift
@@ -0,0 +1,16 @@
+//
+//  SegmentCell.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 21/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+class SegmentCell: UITableViewCell, Reusable{
+    
+    class var reuseIdentifier: String {
+        return "TableViewSegmentCellReuseIdentifier"
+    }
+}
