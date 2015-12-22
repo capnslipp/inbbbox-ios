4d1e47e641b1e389fb34aba8bdd238e057329750
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-22 15:07:26 +0100

    temporary implementation of DatePickerViewController [IOS-69]

---
+++ Inbbbox/Source Files/ViewControllers/DatePickerViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DatePickerViewController.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 21/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+// NGRTemp: temporary implementation
+class DatePickerViewController: UIViewController {
+    
+    private var datePicker = UIDatePicker()
+    
+    private let completion: NSDate -> Void
+    
+    init(date: NSDate, completion: NSDate -> Void) {
+        datePicker.date = date
+        self.completion = completion
+        super.init(nibName: nil, bundle: nil)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = NSLocalizedString("inbbbox", comment: "")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: "didTapSaveButton:")
+    }
+    
+    func didTapSaveButton(sender: UIBarButtonItem) {
+        completion(datePicker.date)
+        navigationController?.popViewControllerAnimated(true)
+    }
+}
+
+// MARK: Private
+
+extension DatePickerViewController {
+    
+    private func commonInit() {
+        
+        view.backgroundColor = UIColor.groupTableViewBackgroundColor()
+        
+        datePicker.backgroundColor = UIColor.whiteColor()
+        datePicker.datePickerMode = .Time
+        datePicker.frame = CGRect(x: 0, y: 64, width: CGRectGetWidth(view.bounds), height: 150) // NGRTemp: temp frame
+        view.addSubview(datePicker)
+    }
+}
