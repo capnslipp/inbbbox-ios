4539126fe4a85df8ec0b64e51d37f0b259773026
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-24 12:10:30 +0100

    RGBA simplified to use [352529554011b3a218206556a5760252e0d1e03e][IOS-69]

--- Inbbbox/Source Files/Extensions/UIColorExtension.swift
+++ Inbbbox/Source Files/Extensions/UIColorExtension.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension UIColor {
     
-    class func RGBA(red255 red: CGFloat, green255 green: CGFloat, blue255 blue: CGFloat, alpha: CGFloat) -> UIColor {
-        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+    class func RGBA(red: Int, _ green: Int, _ blue: Int, _ alpha: CGFloat) -> UIColor {
+        return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: alpha)
     }
     
     class func pinkColor() -> UIColor {
@@ -19,18 +19,18 @@ extension UIColor {
     }
     
     class func pinkColor(alpha alpha: CGFloat) -> UIColor {
-        return RGBA(red255: 240, green255: 55, blue255: 126, alpha: alpha)
+        return RGBA(240, 55, 126, alpha)
     }
     
     class func backgroundGrayColor() -> UIColor {
-        return RGBA(red255: 246, green255: 248, blue255: 248, alpha: 1)
+        return RGBA(246, 248, 248, 1)
     }
     
     class func textDarkColor() -> UIColor {
-        return RGBA(red255: 51, green255: 51, blue255: 51, alpha: 1)
+        return RGBA(51, 51, 51, 1)
     }
     
     class func textLightColor() -> UIColor {
-        return RGBA(red255: 109, green255: 109, blue255: 114, alpha: 1)
+        return RGBA(109, 109, 114, 1)
     }
 }
