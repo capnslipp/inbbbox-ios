577670c9fcd97824b0c8259d78c73eee3dcbb3db
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2015-12-29 13:20:25 +0100

    Additional wrapping closure was redundant [IOS-60]

--- Inbbbox/Source Files/Helpers/ClosureExecutor.swift
+++ Inbbbox/Source Files/Helpers/ClosureExecutor.swift
@@ -8,8 +8,6 @@ class ClosureExecutor {
 
     func executeClosureOnMainThread(delay delay: Double, closure: Void -> Void) {
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            closure()
-        })
+        dispatch_after(time, dispatch_get_main_queue(), closure)
     }
 }
