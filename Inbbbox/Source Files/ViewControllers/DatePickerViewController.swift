f0994a21276711bd6c4b0fb0e07d9c8cf964b302
Peter Bruz <piotr.bruz@netguru.pl>
2015-12-29 09:49:31 +0100

    DatePickerView created [4d1e47e641b1e389fb34aba8bdd238e057329750][IOS-69]

--- Inbbbox/Source Files/ViewControllers/DatePickerViewController.swift
+++ Inbbbox/Source Files/ViewControllers/DatePickerViewController.swift
@@ -8,22 +8,31 @@
 
 import UIKit
 
-// NGRTemp: temporary implementation
 class DatePickerViewController: UIViewController {
     
-    private var datePicker = UIDatePicker()
+    private weak var aView: DatePickerView?
+    private var initialDate: NSDate!
     
-    private let completion: NSDate -> Void
+    private var completion: NSDate -> Void
     
     init(date: NSDate, completion: NSDate -> Void) {
-        datePicker.date = date
+        initialDate = date
         self.completion = completion
+        
         super.init(nibName: nil, bundle: nil)
-        commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        navigationController?.navigationBar.translucent = false
+        
+        aView = loadViewWithClass(DatePickerView.self)
+        aView?.datePicker.date = initialDate
+        
+        view = aView
     }
     
     override func viewDidLoad() {
@@ -34,32 +43,7 @@ class DatePickerViewController: UIViewController {
     }
     
     func didTapSaveButton(sender: UIBarButtonItem) {
-        completion(datePicker.date)
+        completion(aView!.datePicker.date)
         navigationController?.popViewControllerAnimated(true)
-    }
-}
-
-// MARK: Private
-
-extension DatePickerViewController {
-    
-    private func commonInit() {
-        
-        view.backgroundColor = UIColor.backgroundGrayColor()
-        
-        let contentView = UIView(frame: CGRect(x: CGRectGetMinX(view.frame), y: 64, width: CGRectGetWidth(view.frame), height: 217)) // NGRTemp: temp frame
-        contentView.backgroundColor = UIColor.whiteColor()
-        
-        datePicker.backgroundColor = UIColor.whiteColor()
-        datePicker.datePickerMode = .Time
-        datePicker.frame = CGRect(x: 0, y: 20, width: CGRectGetWidth(view.bounds), height: 177) // NGRTemp: temp frame
-        
-        contentView.addSubview(datePicker)
-        
-        let separatorLine = UIView(frame: CGRect(x: 0, y: CGRectGetHeight(contentView.frame) - 0.3, width: CGRectGetWidth(contentView.frame), height: 0.3)) // NGRTemp: temp frame
-        separatorLine.backgroundColor = UIColor.RGBA(224, 224, 224, 1)
-        contentView.addSubview(separatorLine)
-        
-        view.addSubview(contentView)
     }
 }
---
+++ Inbbbox/Source Files/Views/Custom/DatePickerView.swift
@@ -0,0 +1,45 @@
+//
+//  DatePickerView.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 28/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerView: UIView {
+    
+    let datePicker = UIDatePicker()
+    let contentView = UIView()
+    let separatorLine = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+        backgroundColor = UIColor.backgroundGrayColor()
+        
+        let contentView = UIView(frame: CGRect(x: CGRectGetMinX(frame), y: 0, width: CGRectGetWidth(frame), height: 217)) // NGRTemp: temp frame
+        contentView.backgroundColor = UIColor.whiteColor()
+        
+        datePicker.backgroundColor = UIColor.whiteColor()
+        datePicker.datePickerMode = .Time
+        datePicker.frame = CGRect(x: 0, y: 20, width: CGRectGetWidth(bounds), height: 177) // NGRTemp: temp frame
+        
+        let separatorLine = UIView(frame: CGRect(x: 0, y: CGRectGetHeight(contentView.frame) - 1, width: CGRectGetWidth(contentView.frame), height: 1)) // NGRTemp: temp frame
+        separatorLine.backgroundColor = UIColor.RGBA(224, 224, 224, 1)
+        
+        contentView.addSubview(datePicker)
+        contentView.addSubview(separatorLine)
+        addSubview(contentView)
+    }
+}
