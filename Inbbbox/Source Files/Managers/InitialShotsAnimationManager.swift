396c0dc8997e2a0e61bf7746506d40d3854ca02e
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2015-12-29 13:02:13 +0100

    InitialShotsAnimationManager code refactor. Added ClosureExecutor to simplify tests [IOS-60]

--- Inbbbox/Source Files/Managers/InitialShotsAnimationManager.swift
+++ Inbbbox/Source Files/Managers/InitialShotsAnimationManager.swift
@@ -14,65 +14,62 @@ class InitialShotsAnimationManager {
 
     weak var delegate: InitialShotsAnimationManagerDelegate?
     var visibleItems = [AnyObject]()
+    var closureExecutor = ClosureExecutor()
 
 //    Interface
 
     func startAnimationWithCompletion(completion: (Void -> Void)?) {
-//        NGRTemp: temporary implementation
         guard let collectionView = delegate?.collectionViewForAnimationManager(self), items = delegate?.itemsForAnimationManager(self) else {
             return
         }
 
-        let secondMultiplier = 0.1
-
-        for itemIndex in 1 ... items.count {
-            delay(Double(itemIndex) * secondMultiplier) {
-                self.insertNewItem()
-            }
-        }
-
-        delay(1.0 + Double(items.count) * secondMultiplier) {
-
-            for itemIndex in (1 ... items.count - 1).reverse() {
-
-                self.delay(Double(itemIndex) * secondMultiplier) {
-                    if (itemIndex != items.count - 2) {
-                        self.deleteLastItem()
-                    } else {
-                        collectionView.performBatchUpdates({
-                            self.deleteLastItem()
-                        }, completion: {
-                            Bool in
-                            completion?()
-                        })
-                    }
-                }
+        let interval = 0.1
+        addItems(items, collectionView: collectionView, interval: interval) {
+            self.closureExecutor.executeClosureOnMainThread(delay: 1.0) {
+                self.removeItemsWithoutFirstItem(items, collectionView: collectionView, interval: interval, completion: completion)
             }
         }
     }
 
 //    MARK: - Helpers
 
-    private func delay(delay: Double, closure: Void -> Void) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
-    }
-
-    private func insertNewItem() {
-        if let delegate = delegate, collectionView = delegate.collectionViewForAnimationManager(self) {
-            let items = delegate.itemsForAnimationManager(self)
-            let newItemIndex = visibleItems.count
+    private func addItems(items: [AnyObject], collectionView: UICollectionView, interval: Double, completion: (Void -> Void)?) {
+        let addItemAnimation = {
+            let newItemIndex = self.visibleItems.count
             let newItem = items[newItemIndex]
-            visibleItems.append(newItem)
+            self.visibleItems.append(newItem)
             collectionView.insertItemsAtIndexPaths([NSIndexPath(forItem: newItemIndex, inSection: 0)])
         }
+
+        updateItems(items, collectionView: collectionView, interval: interval, animation: addItemAnimation, completion: completion)
     }
 
-    private func deleteLastItem() {
-        if let delegate = delegate, collectionView = delegate.collectionViewForAnimationManager(self) {
-            let items = delegate.itemsForAnimationManager(self)
-            let lastItemIndex = visibleItems.count - 1
-            visibleItems.removeLast()
+    private func removeItemsWithoutFirstItem(items: [AnyObject], collectionView: UICollectionView, interval: Double, completion: (Void -> Void)?) {
+        var reversedItemsWithoutFirstItem = items
+        reversedItemsWithoutFirstItem.removeFirst()
+        reversedItemsWithoutFirstItem.reverse()
+
+        let removeItemAnimation = {
+            let lastItemIndex = self.visibleItems.count - 1
+            self.visibleItems.removeLast()
             collectionView.deleteItemsAtIndexPaths([NSIndexPath(forItem: lastItemIndex, inSection: 0)])
+        }
+
+        self.updateItems(reversedItemsWithoutFirstItem, collectionView: collectionView, interval: interval, animation: removeItemAnimation, completion: completion)
+    }
+
+    private func updateItems(items: [AnyObject], collectionView: UICollectionView, interval: Double, animation: Void -> Void, completion: (Void -> Void)?) {
+        for itemIndex in 1 ... items.count {
+            var updateAnimation = animation
+            if itemIndex == items.count {
+                let batchUpdatesCompletion: (Bool -> Void) = { Bool in
+                    completion?()
+                }
+                updateAnimation = {
+                    collectionView.performBatchUpdates(animation, completion: batchUpdatesCompletion)
+                }
+            }
+            self.closureExecutor.executeClosureOnMainThread(delay: Double(itemIndex) * interval, closure: updateAnimation)
         }
     }
 }
