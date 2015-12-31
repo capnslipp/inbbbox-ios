a520cdd7a85303a0a85fb9c62a4c8c7c8e92d448
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-12-31 13:38:49 +0100

    TokenStorageSpec [IOS-59]

---
+++ Unit Tests/TokenStorageSpec.swift
@@ -0,0 +1,56 @@
+//
+//  TokenStorageSpec.swift
+//  Inbbbox
+//
+//  Created by Patryk Kaczmarek on 31/12/15.
+//  Copyright © 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class TokenStorageSpec: QuickSpec {
+    override func spec() {
+        
+        var savedTokenBeforeTestLaunch: String!
+        
+        beforeSuite {
+            savedTokenBeforeTestLaunch = TokenStorage.currentToken
+        }
+        
+        afterSuite {
+            TokenStorage.storeToken(savedTokenBeforeTestLaunch)
+        }
+        
+        beforeEach {
+            TokenStorage.clear()
+        }
+        
+        describe("when storing token") {
+            
+            beforeEach {
+                TokenStorage.storeToken("fixture.token")
+            }
+            
+            it("token should be properly stored") {
+                expect(TokenStorage.currentToken).to(equal("fixture.token"))
+            }
+            
+            context("and clearing storage") {
+                
+                beforeEach {
+                    TokenStorage.clear()
+                }
+                
+                it("token should be nil") {
+                    expect(TokenStorage.currentToken).to(equal(""))
+                }
+            }
+        }
+    }
+}
+
