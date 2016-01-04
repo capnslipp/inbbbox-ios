e9092ad0d8363d246016e0e7e99057f428ceb172
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2016-01-04 14:08:04 +0100

    [FIX] partially fixed commit

--- Unit Tests/ClosureExecutorSpec.swift
+++ Unit Tests/ClosureExecutorSpec.swift
@@ -27,20 +27,25 @@ class ClosureExecutorSpec: QuickSpec {
         describe("when executing closure after delay") {
             
             var didExecuteClosure = false
+            var isMainThread = false
             
             beforeEach {
                 
                 waitUntil { done in
-                    sut.executeClosureOnMainThread(delay: 0.1, closure: {
+                    sut.executeClosureOnMainThread(delay: 0.1) {
                         didExecuteClosure = true
+                        isMainThread = NSThread.isMainThread()
                         done()
-                    })
+                    }
                 }
-                
             }
             
             it("closure should be executed") {
                 expect(didExecuteClosure).to(beTruthy())
+            }
+            
+            it("closure should be executed on main thread") {
+                expect(isMainThread).to(beTruthy())
             }
         }
     }
