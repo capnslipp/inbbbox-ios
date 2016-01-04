507ddc179eb49d379cfd04e0261a5ad0c6a8eede
Peter Bruz <piotr.bruz@netguru.pl>
2016-01-04 08:03:45 +0100

    empty LocalNotificationRegistrator added [IOS-70]

---
+++ Unit Tests/LocalNotificationRegistratorSpec.swift
@@ -0,0 +1,19 @@
+//
+//  LocalNotificationRegistratorSpec.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 04/01/16.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class LocalNotificationRegistratorSpec: QuickSpec {
+    
+    override func spec() {
+        // NGRTodo: implement me!
+    }
+}
