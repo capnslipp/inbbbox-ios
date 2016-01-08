1981ccce8f2943ad6aca70a6bb5d9a4df79cfc40
Peter Bruz <piotr.bruz@netguru.pl>
2016-01-08 14:55:31 +0100

    added specs for newly created queries [IOS-65]

---
+++ Unit Tests/AddToBucketQuerySpec.swift
@@ -0,0 +1,51 @@
+//
+//  AddToBucketQuerySpec.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 08/01/16.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class AddToBucketQuerySpec: QuickSpec {
+    override func spec() {
+        
+        describe("when newly initialized with shot and bucket identifiers") {
+            
+            var sut: AddToBucketQuery!
+            let fixtureShotID = 123
+            let fixtureBucketID = 234
+            
+            beforeEach {
+                sut = AddToBucketQuery(shotID: fixtureShotID, bucketID: fixtureBucketID)
+            }
+            
+            it("should have PUT method") {
+                expect(sut.method.rawValue).to(equal(Method.PUT.rawValue))
+            }
+            
+            it("should have path with identifier") {
+                expect(sut.path).to(equal("/buckets/234/shots"))
+            }
+            
+            it("should have dribbble service") {
+                expect(sut.service is DribbbleNetworkService).to(beTrue())
+            }
+            
+            it("should have parameters with JSON encoding") {
+                expect(sut.parameters.encoding).to(equal(Parameters.Encoding.JSON))
+            }
+            
+            it("should have one parameter - shot_id") {
+                expect(sut.parameters.body).to(equal(try! NSJSONSerialization.dataWithJSONObject(["shot_id":fixtureShotID], options: .PrettyPrinted)))
+            }
+           
+        }
+        
+    }
+}
+
