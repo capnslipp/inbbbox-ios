00e4af5fa0b0c0ec4b5b2aaab437402e05b0a901
Peter Bruz <piotr.bruz@netguru.pl>
2016-01-11 12:32:06 +0100

    BucketsLocalStorageSpec added [IOS-65]

---
+++ Unit Tests/BucketsLocalStorageSpec.swift
@@ -0,0 +1,45 @@
+//
+//  BucketsLocalStorageSpec.swift
+//  Inbbbox
+//
+//  Created by Peter Bruz on 11/01/16.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class BucketsLocalStorageSpec: QuickSpec {
+    
+    override func spec() {
+        
+        let sut = BucketsLocalStorage(managedContext: setUpInMemoryManagedObjectContext())
+        
+        describe("when creating bucket") {
+            
+            let fixtureBucketId = 1
+            let fixtureBucketName = "fixture.bucket.name"
+            
+            beforeEach {
+                try! sut.create(bucketID: fixtureBucketId, name: fixtureBucketName)
+            }
+            
+            it("there should be 1 bucket") {
+                expect(sut.buckets.count).to(equal(1))
+            }
+            
+            describe("when destroying bucket") {
+                
+                beforeEach {
+                    try! sut.destroy(bucketID: fixtureBucketId)
+                }
+                
+                it("there shouldn't be any bucket") {
+                    expect(sut.buckets.count).to(equal(0))
+                }
+            }
+        }
+    }
+}
