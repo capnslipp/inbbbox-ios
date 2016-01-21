dcab0b413bd881cd52ffa7a1ccd2d1063429b4e2
Lukasz Pikor <vonpik@gmail.com>
2016-01-21 13:56:31 +0100

    Fix for cbe5086: separate view (ShotDetailsView) with view controller (ShotDetailsViewController)

--- Inbbbox/Source Files/ViewControllers/ShotDetailsViewController.swift
+++ Inbbbox/Source Files/ViewControllers/ShotDetailsViewController.swift
@@ -16,45 +16,26 @@ protocol ShotDetailsViewControllerDelegate: class {
 class ShotDetailsViewController: UIViewController {
     
     weak var delegate: ShotDetailsViewControllerDelegate?
-    var closeButton = UIButton()
+    private weak var aView: ShotDetailsView?
+    
+    override func loadView() {
+        aView = loadViewWithClass(ShotDetailsView.self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupUI()
-        setupButtonsInteractions()
-    }
-    
-    // MARK: Button interactions
-    @objc private func closeButtonTapped() {
-        delegate?.didFinishPresentingDetails(self)
-    }
-    
-    // MARK: UI
-    private func setupUI() {
-        view.backgroundColor = UIColor.clearColor()
-        
-        let blurEffect = UIBlurEffect(style: .Light)
-        let blur = UIVisualEffectView(effect: blurEffect)
-        
-        view.addSubview(blur)
-        blur.autoPinEdgesToSuperviewEdges()
-    }
-
-    private func setupButtonsInteractions() {
         setupCloseButton()
     }
     
     private func setupCloseButton() {
-        /* NGRTemp: Later, this button will be part of, probably, tableViewHeader.*/
-        closeButton = UIButton(forAutoLayout: ())
-        closeButton.setTitle("Close", forState: .Normal)
-        closeButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        view.addSubview(closeButton)
-        
-        closeButton.autoPinEdge(.Top, toEdge: .Top, ofView: view, withOffset: 44)
-        closeButton.autoPinEdge(.Right, toEdge: .Right, ofView: view, withOffset: -20.0)
-        
-        closeButton.addTarget(self, action: "closeButtonTapped", forControlEvents: .TouchUpInside)
+        aView?.closeButton.addTarget(self, action: "closeButtonTapped", forControlEvents: .TouchUpInside)
+    }
+}
+
+extension ShotDetailsViewController {
+    
+    @objc private func closeButtonTapped() {
+        delegate?.didFinishPresentingDetails(self)
     }
 }
---
+++ Inbbbox/Source Files/Views/Controllers View/ShotDetailsView.swift
@@ -0,0 +1,45 @@
+//
+//  ShotDetailsView.swift
+//  Inbbbox
+//
+//  Created by Lukasz Pikor on 21.01.2016.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShotDetailsView: UIView {
+    
+    let closeButton = UIButton(forAutoLayout: ())
+    let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+    
+    private var didSetConstraints = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.clearColor()
+        
+        closeButton.setTitle("Close", forState: .Normal)
+        closeButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+
+        addSubview(blurView)
+        addSubview(closeButton)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        if !didSetConstraints {
+            closeButton.autoPinEdge(.Top, toEdge: .Top, ofView: self, withOffset: 44)
+            closeButton.autoPinEdge(.Right, toEdge: .Right, ofView: self, withOffset: -20.0)
+            
+            blurView.autoPinEdgesToSuperviewEdges()
+            didSetConstraints = true
+        }
+        super.updateConstraints()
+    }
+}
