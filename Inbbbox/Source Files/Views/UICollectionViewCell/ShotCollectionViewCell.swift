aadf91826e4bb136f3fd884b4100baf7fd452835
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2016-01-22 14:08:29 +0100

    [WIP] Started working on ShotCollectionViewCell. Added temporary shot image [IOS-80]

--- Inbbbox/Source Files/Views/UICollectionViewCell/ShotCollectionViewCell.swift
+++ Inbbbox/Source Files/Views/UICollectionViewCell/ShotCollectionViewCell.swift
@@ -4,10 +4,12 @@
 
 import UIKit
 
-class ShotCollectionViewCell: UICollectionViewCell, HeightAware, Reusable {
+class ShotCollectionViewCell: UICollectionViewCell {
 
-//    MARK: - Life cycle
+    let shotImageView = UIImageView.newAutoLayoutView()
+    private var didSetConstraints = false
 
+    // MARK: - Life cycle
     @available(*, unavailable, message="Use init(frame:) instead")
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -16,19 +18,36 @@ class ShotCollectionViewCell: UICollectionViewCell, HeightAware, Reusable {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-//        NGRTemp: temporary implementation
-        contentView.backgroundColor = UIColor(red: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                green: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                blue: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                alpha: 1)
+        shotImageView.image = UIImage(named: "shot-menu")
+        contentView.addSubview(shotImageView)
     }
 
-//    MARK: - HeightAware
+    // MARK: - UIView
+
+    override class func requiresConstraintBasedLayout() -> Bool{
+        return true
+    }
+
+    override func updateConstraints() {
+
+        if !didSetConstraints {
+            shotImageView.autoPinEdgesToSuperviewEdges()
+            didSetConstraints = true
+        }
+
+        super.updateConstraints()
+    }
+}
+
+extension ShotCollectionViewCell: HeightAware {
+
     static var prefferedHeight: CGFloat {
         return CGFloat(240)
     }
+}
 
-//    MARK: - Reusable
+extension ShotCollectionViewCell: Reusable {
+
     static var reuseIdentifier: String {
         return "ShotCollectionViewCellIdentifier"
     }
