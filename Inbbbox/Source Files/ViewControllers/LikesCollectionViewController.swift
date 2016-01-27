875520b806cf90f99824cb32d490813f385d19b3
Aleksander Popko <aleksander.popko@netguru.pl>
2016-01-27 12:52:01 +0100

    LikesCollectionViewController uses 2 layouts
    [IOS-95]

--- Inbbbox/Source Files/ViewControllers/LikesCollectionViewController.swift
+++ Inbbbox/Source Files/ViewControllers/LikesCollectionViewController.swift
@@ -13,11 +13,14 @@ class LikesCollectionViewController: UICollectionViewController {
     // MARK: - Lifecycle
     
     // NGRTemp: temporary implementation - remove after adding real likes
-    var likes = ["like1", "like2", "like3", "like4", "like5", "like6", "like7"];
+    var likes = ["like1", "like2", "like3", "like4", "like5", "like6", "like7"]
+    
+    var oneColumnLayoutButton: UIBarButtonItem?
+    var twoColumnsLayoutButton: UIBarButtonItem?
     
     convenience init() {
         let flowLayout = TwoColumnsCollectionViewFlowLayout()
-        flowLayout.itemHeightToWidthRatio = LikeCollectionViewCell.heightToWidthRatio;
+        flowLayout.itemHeightToWidthRatio = LikeCollectionViewCell.heightToWidthRatio
         self.init(collectionViewLayout: flowLayout)
         title = NSLocalizedString("Likes", comment:"")
     }
@@ -29,13 +32,15 @@ class LikesCollectionViewController: UICollectionViewController {
         }
         collectionView.backgroundColor = UIColor.backgroundGrayColor()
         collectionView.registerClass(LikeCollectionViewCell.self, type: .Cell)
+        setupBarButtons()
+        updateBarButtons(collectionView.collectionViewLayout)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.translucent = true
     }
-    
+
     // MARK: UICollectionViewDataSource
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -50,5 +55,48 @@ class LikesCollectionViewController: UICollectionViewController {
     
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         // NGRTodo: present like details view controller
+    }
+    
+    // MARK: Configuration
+    
+    func setupBarButtons() {
+        // NGRTodo: set images instead of title
+        oneColumnLayoutButton = UIBarButtonItem(title: "1", style: .Plain, target: self, action: "didTapOneColumnLayoutButton:")
+        twoColumnsLayoutButton = UIBarButtonItem(title: "2", style: .Plain, target: self, action: "didTapTwoColumnsLayoutButton:")
+        if let firstBarButton = oneColumnLayoutButton, secondBarButton = twoColumnsLayoutButton {
+            navigationItem.rightBarButtonItems = [firstBarButton, secondBarButton]
+        }
+    }
+    
+    func updateBarButtons(layout: UICollectionViewLayout) {
+        if layout.isKindOfClass(OneColumnCollectionViewFlowLayout) {
+            oneColumnLayoutButton?.enabled = false
+            twoColumnsLayoutButton?.enabled = true
+        } else {
+            oneColumnLayoutButton?.enabled = true
+            twoColumnsLayoutButton?.enabled = false
+        }
+    }
+    
+    // MARK: Actions:
+    
+    func didTapOneColumnLayoutButton(_: UIBarButtonItem) {
+        guard let collectionView = collectionView else {
+            return
+        }
+        let flowLayout = OneColumnCollectionViewFlowLayout()
+        flowLayout.itemHeightToWidthRatio = LikeCollectionViewCell.heightToWidthRatio
+        collectionView.setCollectionViewLayout(flowLayout, animated: false)
+        updateBarButtons(collectionView.collectionViewLayout)
+    }
+    
+    func didTapTwoColumnsLayoutButton(_: UIBarButtonItem) {
+        guard let collectionView = collectionView else {
+            return
+        }
+        let flowLayout = TwoColumnsCollectionViewFlowLayout()
+        flowLayout.itemHeightToWidthRatio = LikeCollectionViewCell.heightToWidthRatio
+        collectionView.setCollectionViewLayout(flowLayout, animated: false)
+        updateBarButtons(collectionView.collectionViewLayout)
     }
 }
