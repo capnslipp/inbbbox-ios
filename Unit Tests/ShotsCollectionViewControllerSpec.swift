c3252e0371876e914c27d8e93bc22b5921c014d4
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2016-02-15 11:10:06 +0100

    Removed outdated tests.

--- Unit Tests/ShotsCollectionViewControllerSpec.swift
+++ Unit Tests/ShotsCollectionViewControllerSpec.swift
@@ -63,62 +63,6 @@ class ShotsCollectionViewControllerSpec: QuickSpec {
             }
         }
 
-        // NGRTodo: Update this spec
-        pending("implementation changed") {
-            describe("view did appear") {
-
-                var didStartAnimationWithCompletion: Bool!
-                var capturedCompletion: (() -> ())!
-
-                beforeEach {
-                    let _ = sut.view
-                    let animationManagerMock = ShotsAnimatorMock()
-                    didStartAnimationWithCompletion = false
-                    animationManagerMock.startAnimationWithCompletionStub.on(any()) { completion in
-                        didStartAnimationWithCompletion = true
-                        capturedCompletion = completion
-                    }
-                    sut.animationManager = animationManagerMock
-                    sut.viewDidAppear(true)
-                }
-
-                it("should start animation with completion") {
-                    expect(didStartAnimationWithCompletion).to(beTruthy())
-                }
-
-                describe("animation completion") {
-
-                    var didReloadCollectionViewData: Bool!
-                    var tabBarController: UITabBarController!
-
-                    beforeEach {
-                        tabBarController = UITabBarController()
-                        tabBarController.viewControllers = [sut]
-
-                        let collectionViewMock = CollectionViewMock()
-                        didReloadCollectionViewData = false
-                        collectionViewMock.reloadDataStub.on(any()) {
-                            didReloadCollectionViewData = true
-                        }
-                        sut.collectionView = collectionViewMock
-                        capturedCompletion()
-                    }
-
-                    it("should change collection view layout to flow layout") {
-                        expect(sut.collectionView!.collectionViewLayout).to(beAKindOf(ShotsCollectionViewFlowLayout))
-                    }
-
-                    it("should reload collection view data") {
-                        expect(didReloadCollectionViewData).to(beTruthy())
-                    }
-
-                    it("should have user interaction enabled on tab bar") {
-                        expect(sut.tabBarController!.tabBar.userInteractionEnabled).to(beTruthy())
-                    }
-                }
-            }
-        }
-
         describe("collection view data source") {
 
             describe("number of items in section") {
@@ -135,26 +79,6 @@ class ShotsCollectionViewControllerSpec: QuickSpec {
 
                     it("should return number of visible items provided by animation manager") {
                         expect(numberOfItems).to(equal(2))
-                    }
-                }
-
-                // NGRTodo: Update this spec
-                pending("implementation changed") {
-                    context("when animations did finish") {
-
-                        beforeEach {
-                            let animationManagerMock = ShotsAnimatorMock()
-                            animationManagerMock.startAnimationWithCompletionStub.on(any()) { completion in
-                                completion?()
-                            }
-                            sut.animationManager = animationManagerMock
-                            sut.viewDidAppear(true)
-                            numberOfItems = sut.collectionView(CollectionViewMock(), numberOfItemsInSection: 0)
-                        }
-
-                        it("should return number of shots") {
-                            expect(numberOfItems).to(equal(10))
-                        }
                     }
                 }
             }
@@ -231,64 +155,6 @@ class ShotsCollectionViewControllerSpec: QuickSpec {
                             }
                         }
                     }
-                }
-            }
-        }
-
-        describe("scroll view delegate") {
-
-            describe("scroll view did scroll") {
-
-                var shotCell: ShotCollectionViewCell!
-                var capturedImageURL: NSURL!
-                var capturedPlaceholderImage: UIImage!
-                var capturedBlur: CGFloat!
-                var blurredImage: UIImage!
-
-                beforeEach {
-                    shotCell = ShotCollectionViewCell(frame: CGRectZero)
-                    let scrollView = UIScrollView()
-                    scrollView.bounds = CGRect(x: 0, y: 0, width: 0, height: 100)
-                    scrollView.contentOffset = CGPoint(x: 0, y: 110)
-                    let collectionViewMock = CollectionViewMock()
-                    collectionViewMock.visibleCellsStub.on(any()) {
-                        return [shotCell]
-                    }
-                    collectionViewMock.indexPathForCellStub.on(any()) { _ in
-                        return NSIndexPath(forItem: 0, inSection: 0)
-                    }
-                    sut.collectionView = collectionViewMock
-                    let imageClassMock = ImageMock.self
-                    let imageMock = ImageMock()
-                    blurredImage = UIImage()
-                    imageMock.blurredImageStub.on(any()) { blur in
-                        capturedBlur = blur
-                        return blurredImage
-                    }
-                    imageClassMock.cachedImageFromURLStub.on(any()) { url, placeholderImage in
-                        capturedImageURL = url
-                        capturedPlaceholderImage = placeholderImage
-                        return imageMock
-                    }
-                    sut.imageClass = imageClassMock
-                    sut.shots = [Shot.fixtureShot()]
-                    sut.scrollViewDidScroll(scrollView)
-                }
-
-                it("should get cached image with proper url") {
-                    expect(capturedImageURL).to(equal(NSURL(string:"https://fixture.domain/fixture.image.normal.png")))
-                }
-
-                it("should get chached image with proper placeholder image") {
-                    expect(UIImagePNGRepresentation(capturedPlaceholderImage)).to(equal(UIImagePNGRepresentation(UIImage(named: "shot-menu")!)))
-                }
-
-                it("should blur image with proper value") {
-                    expect(capturedBlur).to(equal(0.2))
-                }
-
-                it("should set proper blurred image on visible cell") {
-                    expect(shotCell.shotImageView.image).to(equal(blurredImage))
                 }
             }
         }
