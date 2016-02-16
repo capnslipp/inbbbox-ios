4bbb50162e6b6ef64ed7de837ce22803d457ccab
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2016-02-16 18:26:57 +0100

    Reflecting changes in pbxproj

---
+++ Unit Tests/ConnectionsRequesterSpec.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectionsRequesterSpec.swift
+//  Inbbbox
+//
+//  Created by Patryk Kaczmarek on 16/02/16.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
