e2afe8521e94401d3a6c02fb7a6313c5f48ae01e
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2016-02-16 17:46:41 +0100

    (Un)follow query and their specs

---
+++ Unit Tests/Queries/FollowUserQuerySpec.swift
@@ -0,0 +1,42 @@
+//
+//  FollowUserQuerySpec.swift
+//  Inbbbox
+//
+//  Created by Patryk Kaczmarek on 16/02/16.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class FollowUserQuerySpec: QuickSpec {
+    override func spec() {
+        
+        SharedQuerySpec.performSpecForQuery( { Void -> Query in
+            return FollowUserQuery(user: User.fixtureUser())
+        }) { Void -> QueryExpectation in
+            return (method: .PUT, encoding: .URL, path: "/users/fixture.identifier/follow")
+        }
+        
+        describe("when newly initialized with user") {
+            
+            var sut: FollowUserQuery!
+            
+            beforeEach {
+                sut = FollowUserQuery(user: User.fixtureUser())
+            }
+            
+            afterEach {
+                sut = nil
+            }
+            
+            it("should have empty parameters") {
+                expect(sut.parameters.queryItems).to(beEmpty())
+            }
+        }
+    }
+}
