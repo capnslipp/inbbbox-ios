e903db38610230a01c8f7d3d2cc798886b88fdfd
Aleksander Popko <aleksander.popko@netguru.pl>
2016-02-25 09:31:15 +0100

    Add initial buckets view model

---
+++ Inbbbox/Source Files/ViewModels/BucketsViewModel.swift
@@ -0,0 +1,111 @@
+//
+//  BucketsViewModel.swift
+//  Inbbbox
+//
+//  Created by Aleksander Popko on 24.02.2016.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+class BucketsViewModel {
+    
+    private var buckets = [BucketType]()
+    //NGRTemp: could be solved nicer with IOS-131
+    private var bucketsIndexedShots = [Int : [ShotType]]()
+    private let bucketsProvider = BucketsProvider()
+    private let shotsProvider = ShotsProvider()
+    
+    private func downloadInitialBuckets(handler: () -> ()) {
+        firstly {
+            bucketsProvider.provideMyBuckets()
+        }.then { buckets -> Void in
+            if let buckets = buckets {
+                self.buckets = buckets
+                self.downloadShots(buckets)
+            }
+            handler()
+        }.error { error in
+            // NGRTemp: Need mockups for error message view
+            print(error)
+        }
+        
+    }
+    
+    func downloadBucketsForNextPage(handler: ([NSIndexPath]) -> ()) {
+        firstly {
+            bucketsProvider.nextPage()
+        }.then { buckets -> Void in
+            if let buckets = buckets where buckets.count > 0 {
+                let indexes = buckets.enumerate().map { index, _ in
+                    return index + self.buckets.count
+                }
+                self.buckets.appendContentsOf(buckets)
+                let indexPaths = indexes.map {
+                    NSIndexPath(forRow:($0), inSection: 0)
+                }
+                handler(indexPaths)
+                //self.collectionView?.insertItemsAtIndexPaths(indexPaths)
+                self.downloadShots(buckets)
+            }
+        }.error { error in
+            // NGRTemp: Need mockups for error message view
+            print(error)
+        }
+    }
+    
+    func downloadShots(buckets: [BucketType]) {
+        for bucket in buckets {
+            firstly {
+                shotsProvider.provideShotsForBucket(bucket)
+            }.then { shots -> Void in
+                var indexOfBucket: Int?
+                for (index, item) in self.buckets.enumerate(){
+                    if item.identifier == bucket.identifier {
+                        indexOfBucket = index
+                        break
+                    }
+                }
+                guard let index = indexOfBucket else {
+                    return
+                }
+                if let shots = shots {
+                    self.bucketsIndexedShots[index] = shots
+                } else {
+                    self.bucketsIndexedShots[index] = [ShotType]()
+                }
+                let indexPath = NSIndexPath(forRow: index, inSection: 0)
+           //     self.collectionView?.reloadItemsAtIndexPaths([indexPath])
+            }.error { error in
+                // NGRTemp: Need mockups for error message view
+                print(error)
+            }
+        }
+    }
+    
+    func bucketCollectionViewCellViewData(indexPath: NSIndexPath) -> BucketCollectionViewCellViewData {
+        return BucketCollectionViewCellViewData(bucket: buckets[indexPath.row], shots: bucketsIndexedShots[indexPath.row])
+    }
+    
+}
+
+extension BucketsViewModel {
+    
+    struct BucketCollectionViewCellViewData {
+        let name: String
+        let numberOfShots: String
+        let shotsImagesURLs: [NSURL]
+        
+        init(bucket: BucketType, shots: [ShotType]?) {
+            self.name = bucket.name
+            self.numberOfShots = bucket.shotsCount == 1 ? "\(bucket.shotsCount) shot" : "\(bucket.shotsCount) shots"
+            if let shots = shots {
+                let allShotsImagesURLs = shots.map { $0.shotImage.normalURL }
+                self.shotsImagesURLs =  Array(Array(Array(count: 4, repeatedValue: allShotsImagesURLs).flatten())[0...3])
+            } else {
+                self.shotsImagesURLs = [NSURL]()
+            }
+        }
+    }
+}
