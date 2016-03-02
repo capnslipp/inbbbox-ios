f6acde91ce0907302b402bf8f782d889b424468c
Marcin <marcin.siemaszko@netguru.pl>
2016-03-02 14:40:20 +0100

    add animatable shot image view

---
+++ Inbbbox/Source Files/Views/Custom/AnimatableShotImageView.swift
@@ -0,0 +1,63 @@
+//
+//  AnimatableShotImageView.swift
+//  Inbbbox
+//
+//  Created by Marcin Siemaszko on 02.03.2016.
+//  Copyright © 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+import KFSwiftImageLoader
+import Gifu
+
+class AnimatableShotImageView: AnimatableImageView {
+    
+    private let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+    private var didSetupConstraints = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .followeeShotGrayColor()
+        contentMode = .ScaleAspectFit
+        
+        activityIndicatorView.color = .whiteColor()
+        addSubview(activityIndicatorView)
+    }
+    
+    @available(*, unavailable, message="Use init(frame:) method instead")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override class func requiresConstraintBasedLayout() -> Bool {
+        return true
+    }
+    
+    override func updateConstraints() {
+        
+        if !didSetupConstraints {
+            didSetupConstraints = true
+            
+            activityIndicatorView.autoCenterInSuperview()
+        }
+        
+        super.updateConstraints()
+    }
+    
+    func loadAnimatableShotFromUrl(url: NSURL) {
+        activityIndicatorView.startAnimating()
+        let request = NSURLRequest(URL: url)
+        NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            dispatch_async(dispatch_get_main_queue(), {
+                if let data = data {
+                    self.animateWithImageData(data)
+                }
+                self.activityIndicatorView.stopAnimating()
+            })
+            }.resume()
+    }
+}
+
