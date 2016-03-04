4c3dcec9411c6685090713469def6d4466fbb747
Peter Bruz <piotr.bruz@netguru.pl>
2016-03-04 11:37:36 +0100

    present shot details after tapping shot in `likes view`

--- Inbbbox/Source Files/ViewControllers/LikesCollectionViewController.swift
+++ Inbbbox/Source Files/ViewControllers/LikesCollectionViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import PromiseKit
+import ZFDragableModalTransition
 
 class LikesCollectionViewController: TwoLayoutsCollectionViewController, BaseCollectionViewViewModelDelegate {
     
     let viewModel = LikesViewModel()
+    var modalTransitionAnimator: ZFModalTransitionAnimator?
     
     // MARK: - Lifecycle
     
@@ -55,7 +57,18 @@ class LikesCollectionViewController: TwoLayoutsCollectionViewController, BaseCol
     }
     
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        // NGRTodo: present like details view controller
+        let shotDetailsViewController = ShotDetailsViewController(shot: viewModel.likedShots[indexPath.item])
+        shotDetailsViewController.modalPresentationStyle = .OverCurrentContext
+        
+        modalTransitionAnimator = ZFModalTransitionAnimator(modalViewController: shotDetailsViewController)
+        modalTransitionAnimator?.dragable = true
+        modalTransitionAnimator?.direction = ZFModalTransitonDirection.Bottom
+        modalTransitionAnimator?.setContentScrollView(shotDetailsViewController.shotDetailsView.collectionView)
+        modalTransitionAnimator?.behindViewAlpha = 0.5
+        shotDetailsViewController.transitioningDelegate = modalTransitionAnimator
+        shotDetailsViewController.modalPresentationStyle = .Custom
+        
+        tabBarController?.presentViewController(shotDetailsViewController, animated: true, completion: nil)
     }
     
     // MARK: Base Collection View View Model Delegate
