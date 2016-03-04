b97cca7aa05f539ad95a997e6703ac6ee738b9be
Marcin <marcin.siemaszko@netguru.pl>
2016-03-04 12:50:38 +0100

    improve blur handling

--- Inbbbox/Source Files/ViewControllers/ShotsCollectionViewController.swift
+++ Inbbbox/Source Files/ViewControllers/ShotsCollectionViewController.swift
@@ -152,9 +152,8 @@ final class ShotsCollectionViewController: UICollectionViewController {
         let blur = min(scrollView.contentOffset.y % CGRectGetHeight(scrollView.bounds), CGRectGetHeight(scrollView.bounds) - scrollView.contentOffset.y % CGRectGetHeight(scrollView.bounds)) / (CGRectGetHeight(scrollView.bounds) / 2)
 
         for cell in collectionView.visibleCells() {
-            if let shotCell = cell as? ShotCollectionViewCell, indexPath = collectionView.indexPathForCell(shotCell) {
-                let shot = shots[indexPath.item]
-                shotCell.shotImageView.loadShotImageFromURL(shot.shotImage.normalURL, blur: blur)
+            if let shotCell = cell as? ShotCollectionViewCell {
+                shotCell.shotImageView.applyBlur(blur)
             }
         }
     }
