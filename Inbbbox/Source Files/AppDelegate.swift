01f5637f5d7517099dd6ae1a4df7a81c51786690
Lukasz Pikor <vonpik@gmail.com>
2016-03-31 14:59:47 +0200

    Integration with HockeyApp Crash Manager.

--- Inbbbox/Source Files/AppDelegate.swift
+++ Inbbbox/Source Files/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         AnalyticsManager.setupAnalytics()
+        CrashManager.setup()
         let rootViewController = UserStorage.isUserSignedIn ? CenterButtonTabBarController() : LoginViewController()
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window!.rootViewController = rootViewController
---
+++ Inbbbox/Source Files/Managers/CrashManager.swift
@@ -0,0 +1,20 @@
+//
+// Created by Lukasz Pikor on 31.03.2016.
+// Copyright (c) 2016 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import HockeySDK
+
+class CrashManager {
+
+    class func setup() {
+        guard let identifier = Dribbble.HockeySDKIdentifier else {
+            return
+        }
+
+        BITHockeyManager.sharedHockeyManager().configureWithIdentifier(identifier)
+        BITHockeyManager.sharedHockeyManager().crashManager.crashManagerStatus = .AutoSend
+        BITHockeyManager.sharedHockeyManager().startManager()
+    }
+}
