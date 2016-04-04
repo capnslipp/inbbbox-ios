43cb1d7676c2d1bb8fe563f3762e9e5dfadf8291
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2016-04-04 14:42:33 +0200

    Acknowledgements are now properly displayed

--- Inbbbox/Source Files/ViewControllers/AcknowledgementsViewController.swift
+++ Inbbbox/Source Files/ViewControllers/AcknowledgementsViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
+import WebKit
 
 class AcknowledgementsViewController: UIViewController {
+    var webView: WKWebView {
+        return view as! WKWebView
+    }
 }
 
 // MARK: UIViewController
@@ -8,15 +12,32 @@ extension AcknowledgementsViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .whiteColor()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "didTapBackButton:")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "didTapBackButton:")
+        webView.loadHTMLString(acknowledgementsHTMLString(), baseURL: nil)
+    }
+
+    override func loadView() {
+        view = WKWebView(frame: CGRectZero)
     }
 }
 
 // MARK: Actions
 extension AcknowledgementsViewController {
-
     func didTapBackButton(_: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+// MARK: Private methods
+private extension AcknowledgementsViewController {
+    func acknowledgementsHTMLString() -> String {
+        guard
+        let file = NSBundle.mainBundle().pathForResource("Acknowledgements", ofType:"html"),
+        let data = NSData(contentsOfFile: file),
+        let htmlString = String(data: data, encoding: NSUTF8StringEncoding)
+        else {
+            return ""
+        }
+        return htmlString
     }
 }
