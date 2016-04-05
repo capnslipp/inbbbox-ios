5373f2ffa88f8c5df90b5e6d26925aaec0884a69
Lukasz Wolanczyk <wolanczyk.lukasz@gmail.com>
2016-04-05 14:56:24 +0200

    Adding documentation to ShotType

--- Inbbbox/Source Files/Model/Protocols/ShotType.swift
+++ Inbbbox/Source Files/Model/Protocols/ShotType.swift
@@ -10,10 +10,27 @@ import Foundation
 
 protocol ShotType {
     var identifier: String { get }
+
+    /// The title of the shot
     var title: String { get }
+
+    /// A description of the shot
     var attributedDescription: NSAttributedString? { get }
+
+    /**
+     Owner of this shot
+     
+     - returns: User
+     */
     var user: UserType { get }
+
+    /**
+     Shot image can be a GIF, JPG, or PNG
+     
+     - returns: Shot image with available URLs
+     */
     var shotImage: ShotImageType { get }
+    
     var createdAt: NSDate { get }
     var animated: Bool { get }
     var likesCount: UInt { get }
@@ -28,16 +45,16 @@ func ==(lhs: ShotType, rhs: ShotType) -> Bool {
 }
 
 func ==(lhs: [ShotType], rhs: [ShotType]) -> Bool {
-    
+
     guard lhs.count == rhs.count else { return false }
-    
+
     var indexingGenerators = (left: lhs.generate(), right: rhs.generate())
-    
+
     var isEqual = true
     while let leftElement = indexingGenerators.left.next(), rightElement = indexingGenerators.right.next() where isEqual {
         isEqual = leftElement == rightElement
     }
-    
+
     return isEqual
 }
 
