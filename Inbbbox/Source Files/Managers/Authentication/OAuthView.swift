d2822658d9fe24189414911f7f20560cd302b9aa
Lukasz Pikor <vonpik@gmail.com>
2016-05-12 12:08:47 +0200

    Allow to navigate using UIToolbar.

--- Inbbbox/Source Files/Managers/Authentication/OAuthView.swift
+++ Inbbbox/Source Files/Managers/Authentication/OAuthView.swift
@@ -13,6 +13,9 @@ import PureLayout
 final class OAuthView: UIView {
 
     let webView: WKWebView
+    let back: UIBarButtonItem
+    let forward: UIBarButtonItem
+    private let toolbar = UIToolbar.newAutoLayoutView()
     private var didSetConstraints = false
 
     override init(frame: CGRect) {
@@ -29,9 +32,23 @@ final class OAuthView: UIView {
         webView.backgroundColor = .grayColor()
         webView.allowsBackForwardNavigationGestures = true
 
+        back = UIBarButtonItem(image: UIImage(named: "ic-back"), style: .Plain, target: nil, action: nil)
+        back.enabled = false
+
+        forward = UIBarButtonItem(image: UIImage(named: "ic-forward"), style: .Plain, target: nil, action: nil)
+        forward.enabled = false
+
+        let space = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+
+        toolbar.setItems([space, back, space, forward, space], animated: true)
+        toolbar.barStyle = .Default
+        toolbar.tintColor = .whiteColor()
+        toolbar.barTintColor = .pinkColor()
+
         super.init(frame: frame)
 
         addSubview(webView)
+        addSubview(toolbar)
         backgroundColor = .whiteColor()
     }
 
@@ -46,6 +63,9 @@ final class OAuthView: UIView {
             didSetConstraints = true
 
             webView.autoPinEdgesToSuperviewEdges()
+            toolbar.autoPinEdgeToSuperviewEdge(.Leading)
+            toolbar.autoPinEdgeToSuperviewEdge(.Trailing)
+            toolbar.autoPinEdgeToSuperviewEdge(.Bottom)
         }
 
         super.updateConstraints()
