72ee5bc5b3a61076a7714b9490733b58488f15da
Błażej Wdowikowski <blazej@wdowikowski.pl>
2016-10-28 10:50:23 +0200

    Fixed test

--- Unit Tests/ShotsCollectionViewFlowLayoutSpec.swift
+++ Unit Tests/ShotsCollectionViewFlowLayoutSpec.swift
@@ -14,6 +14,7 @@ class ShotsCollectionViewFlowLayoutSpec: QuickSpec {
         var collectionView: UICollectionView?
 
         beforeEach {
+            
             sut = ShotsCollectionViewFlowLayout()
             collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: sut!)
         }
@@ -22,9 +23,10 @@ class ShotsCollectionViewFlowLayoutSpec: QuickSpec {
             sut = nil
         }
 
-        describe("prepare layout") {
+        describe("prepare layout without author's name showed") {
 
             beforeEach {
+                Settings.Customization.ShowAuthor = false
                 collectionView!.bounds = CGRect(x: 0, y: 0, width: 375, height: 667)
                 sut!.prepareLayout()
             }
@@ -44,6 +46,34 @@ class ShotsCollectionViewFlowLayoutSpec: QuickSpec {
 
             it("should have proper section inset") {
                 expect(sut!.sectionInset).to(equal(UIEdgeInsets(top: 214, left: 0, bottom: 214, right: 0)))
+            }
+        }
+        
+        describe("prepare layout with author's name showed") {
+            
+            beforeEach {
+                Settings.Customization.ShowAuthor = true
+                collectionView!.bounds = CGRect(x: 0, y: 0, width: 375, height: 667)
+                sut!.prepareLayout()
+            }
+            
+            it("should have proper item size") {
+                expect(sut!.itemSize.width).to(equal(320))
+                expect(Int(sut!.itemSize.height)).to(equal(265))
+            }
+            
+            it("should have proper item height to width ratio") {
+                let heightToWidhtRatio = sut!.itemSize.height / sut!.itemSize.width
+                
+                expect(heightToWidhtRatio).to(beCloseTo(0.83))
+            }
+            
+            it("should have proper minimum line spacing") {
+                expect(Int(sut!.minimumLineSpacing)).to(equal(401))
+            }
+            
+            it("should have proper section inset") {
+                expect(sut!.sectionInset).to(equal(UIEdgeInsets(top: 201, left: 0, bottom: 201, right: 0)))
             }
         }
     }
