1536cc73934ec5cf21a97d2e95d9082c16deb852
Marcin Siemaszko <siemman@gmail.com>
2016-11-03 11:38:13 +0100

    use Async wrapper

--- Inbbbox/Source Files/ViewControllers/ShotsCollectionViewController.swift
+++ Inbbbox/Source Files/ViewControllers/ShotsCollectionViewController.swift
@@ -80,10 +80,9 @@ extension ShotsCollectionViewController {
         AnalyticsManager.trackScreen(.ShotsView)
 
         if(onceTokenForInitialShotsAnimation != 0) {
-            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
-            dispatch_after(delayTime, dispatch_get_main_queue()) { [weak self] in
-                self?.showStreamSources()
-            }
+            AsyncWrapper().main(after: 1, block: { [unowned self] in
+                self.showStreamSources()
+            })
         }
         dispatch_once(&onceTokenForInitialShotsAnimation) {
             firstly {
@@ -313,9 +312,8 @@ private extension ShotsCollectionViewController {
     
     func showStreamSources() {
         backgroundAnimator?.startFadeInAnimation()
-        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(4 * Double(NSEC_PER_SEC)))
-        dispatch_after(delayTime, dispatch_get_main_queue()) { [weak self] in
-            self?.backgroundAnimator?.startFadeOutAnimation()
+        AsyncWrapper().main(after: 4) { [unowned self] in
+            self.backgroundAnimator?.startFadeOutAnimation()
         }
     }
     
